0a79ac670cd16ba653d6b15723155a82dbcc0334
EpicDoomSir <eitands@gmail.com>
2021-02-04 06:02:34 -0500

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Family.destroy_all
+UserFamily.destroy_all
+Fridge.destroy_all
+Shoppinglist.destroy_all
+
+user1 = User.create(username: "bob", password: "bob")
+user2 = User.create(username: "beth", password: "beth")
+user3 = User.create(username: "bart", password: "bart")
+
+fam1 = Family.create(name: "smith", password: "smith")
+fam2 = Family.create(name: "parker", password: "parker")
+
+user_fam1 = UserFamily.create(user_id: user1.id, family_id: fam1.id)
+user_fam2 = UserFamily.create(user_id: user2.id, family_id: fam2.id)
+user_fam3 = UserFamily.create(user_id: user3.id, family_id: fam1.id)
+user_fam4 = UserFamily.create(user_id: user3.id, family_id: fam2.id)
+
+fridge1 = Fridge.create(name: "kitchen", family_id: fam1.id)
+fridge2 = Fridge.create(name: "kieth", family_id: fam2.id)
+
+shoppinglist1 = Shoppinglist.create(name: "groceries", family_id: fam1.id)
+shoppinglist2 = Shoppinglist.create(name: "garth", family_id: fam2.id)
