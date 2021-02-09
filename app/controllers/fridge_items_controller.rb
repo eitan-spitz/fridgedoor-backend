654633a1f263227f7730983d25e182d03ae4c593
EpicDoomSir <eitands@gmail.com>
2021-02-09 09:23:34 -0500

    items deleting correctly

--- app/controllers/fridge_items_controller.rb
+++ app/controllers/fridge_items_controller.rb
@@ -1,2 +1,7 @@
 class FridgeItemsController < ApplicationController
+    def destroy
+        fridge_item = FridgeItem.find_by(id: params[:id])
+        fridge_item.destroy
+        render json: { success: 'FridgeItem deleted successfully' }
+    end
 end
--- app/serializers/fridge_item_serializer.rb
+++ app/serializers/fridge_item_serializer.rb
@@ -1,3 +1,6 @@
 class FridgeItemSerializer < ActiveModel::Serializer
   attributes :id, :fridge_id, :item_id
+
+  belongs_to :fridge, serializer: FridgeSerializer
+  belongs_to :item, serializer: ItemSerializer
 end
