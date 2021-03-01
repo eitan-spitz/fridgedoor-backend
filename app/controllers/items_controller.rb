ea1bd55c1d9222d75fbeca7c2df6ac59c943172c
EpicDoomSir <eitands@gmail.com>
2021-03-01 07:11:48 -0500

    item create route

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,18 @@
 class ItemsController < ApplicationController
+
+    def create
+        byebug
+        item = Item.find_by(fdc_id: item_params[:fdc_id])
+        if item 
+            render json: item
+        else
+            new_item = Item.create!(item_params)
+            render json: new_item
+        end
+    end
+
+    private
+    def item_params
+        params.require(:item).permit(:fdc_id, :fdc_description, :data_type)
+    end
 end
